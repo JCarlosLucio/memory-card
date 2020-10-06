53129afadd77170aabaa1a0d318f05be0df66554
jcLucio <c.lucio3@gmail.com>
2020-10-06 12:57:15 -0500

    Add img element

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function Card() {
-  return <div>Card</div>;
+function Card({ id, img }) {
+  return (
+    <div>
+      <img
+        src={`https://source.unsplash.com/${img}/250x350`}
+        alt={`dog-${id}`}
+      />
+    </div>
+  );
 }
 
 export default Card;
