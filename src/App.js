ac93711acd11b92b8d995e6f7b1933c5733acbf7
jcLucio <c.lucio3@gmail.com>
2020-10-06 11:47:09 -0500

    Add cards state w/initialCards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const initialCards = [
+    { id: 0, imgSrc: '' },
+    { id: 1, imgSrc: '' },
+    { id: 2, imgSrc: '' },
+    { id: 3, imgSrc: '' },
+    { id: 4, imgSrc: '' },
+    { id: 5, imgSrc: '' },
+    { id: 6, imgSrc: '' },
+    { id: 7, imgSrc: '' },
+    { id: 8, imgSrc: '' },
+    { id: 9, imgSrc: '' },
+    { id: 10, imgSrc: '' },
+    { id: 11, imgSrc: '' },
+  ];
+
+  const [cards, setCards] = useState(initialCards);
+
   return (
     <div>
       <header>
