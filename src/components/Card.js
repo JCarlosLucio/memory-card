79070adae5c4205a83071b7b6ed7b9eb579022fd
jcLucio <c.lucio3@gmail.com>
2020-10-06 15:53:26 -0500

    Add root style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Card.module.scss';
 
 function Card({ id, img }) {
   return (
-    <div>
+    <div className={styles.root}>
       <img
         src={`https://source.unsplash.com/${img}/250x350`}
         alt={`dog-${id}`}
