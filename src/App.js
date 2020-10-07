0d64ccbf3b793e63eecba1f0a39edf88c56b0dbc
jcLucio <c.lucio3@gmail.com>
2020-10-07 10:57:12 -0500

    Refactor initialCards to seedCards.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,10 @@ import React, { useEffect, useState } from 'react';
 import useShuffleState from './hooks/useShuffleState';
 import Header from './components/Header';
 import CardGrid from './components/CardGrid';
+import seedCards from './seedCards';
 
 function App() {
-  const initialCards = [
-    { id: 0, img: 'dpb-qhtcA0A' },
-    { id: 1, img: 'tFiXn-Rjmb4' },
-    { id: 2, img: 'qNJpGSCv_Jc' },
-    { id: 3, img: 'v0_MCllHY9M' },
-    { id: 4, img: '2glK9ao5jYI' },
-    { id: 5, img: '11lRpYcvbrk' },
-    { id: 6, img: 'KZv7w34tluA' },
-    { id: 7, img: '09iA8GzINiI' },
-    { id: 8, img: 'IE2Z11zKsso' },
-    { id: 9, img: '7WF-9GaU-cM' },
-    { id: 10, img: 'xZ5E9H5DIi0' },
-    { id: 11, img: '80e_Gtz58Ks' },
-  ];
-
-  const [cards, shuffleCards] = useShuffleState(initialCards);
+  const [cards, shuffleCards] = useShuffleState(seedCards);
   const [score, setScore] = useState([]);
   const [topScore, setTopScore] = useState(0);
 
@@ -38,7 +24,7 @@ function App() {
     } else {
       setScore([...score, id]);
     }
-    shuffleCards(initialCards);
+    shuffleCards(seedCards);
   };
 
   return (
---
+++ src/seedCards.js
@@ -0,0 +1,14 @@
+export default [
+  { id: 0, img: 'dpb-qhtcA0A' },
+  { id: 1, img: 'tFiXn-Rjmb4' },
+  { id: 2, img: 'qNJpGSCv_Jc' },
+  { id: 3, img: 'v0_MCllHY9M' },
+  { id: 4, img: '2glK9ao5jYI' },
+  { id: 5, img: '11lRpYcvbrk' },
+  { id: 6, img: 'KZv7w34tluA' },
+  { id: 7, img: '09iA8GzINiI' },
+  { id: 8, img: 'IE2Z11zKsso' },
+  { id: 9, img: '7WF-9GaU-cM' },
+  { id: 10, img: 'xZ5E9H5DIi0' },
+  { id: 11, img: '80e_Gtz58Ks' },
+];
