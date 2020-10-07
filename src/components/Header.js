f4bc6f08bd8e84e38173a53b6a233b8bf5445c24
jcLucio <c.lucio3@gmail.com>
2020-10-06 21:41:09 -0500

    Update scores elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,15 @@ import styles from './Header.module.scss';
 function Header({ score, topScore }) {
   return (
     <header className={styles.root}>
-      <h1>Memory Card</h1>
-      <h3>Score: {score}</h3>
-      <h3>Top Score: {topScore}</h3>
+      <h1 className={styles.title}>Memory Card</h1>
+      <div className={styles.score}>
+        <h3>Score</h3>
+        <h2>{score}</h2>
+      </div>
+      <div className={styles.score}>
+        <h3>Top Score</h3>
+        <h2>{topScore}</h2>
+      </div>
     </header>
   );
 }
