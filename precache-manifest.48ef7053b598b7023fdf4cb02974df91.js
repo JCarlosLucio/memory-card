af402de2ca3c314df60c83e25f9727900df0cfc4
jcLucio <c.lucio3@gmail.com>
2020-10-07 11:27:28 -0500

    Updates

---
+++ precache-manifest.48ef7053b598b7023fdf4cb02974df91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fd3d0ee86c93aeefdb1c3d43d2e61b",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "080d50770028c5b85f32",
+    "url": "/memory-card/static/css/main.e87b7c0f.chunk.css"
+  },
+  {
+    "revision": "1908c6adc173f307d511",
+    "url": "/memory-card/static/js/2.3c149df9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card/static/js/2.3c149df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080d50770028c5b85f32",
+    "url": "/memory-card/static/js/main.60f9fe16.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  }
+]);
