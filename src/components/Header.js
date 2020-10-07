d8ad6a3032fdbd06cb4ce1648ed890b6f7d75bb6
jcLucio <c.lucio3@gmail.com>
2020-10-06 21:22:59 -0500

    Add topScore to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Header.module.scss';
 
-function Header({ score }) {
+function Header({ score, topScore }) {
   return (
     <header className={styles.root}>
       <h1>Memory Card</h1>
       <h3>Score: {score}</h3>
-      <h3>Top Score: 0</h3>
+      <h3>Top Score: {topScore}</h3>
     </header>
   );
 }
